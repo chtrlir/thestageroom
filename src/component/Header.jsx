c79c594a66302f27e8e21795bf56160741df9493
chtrlir <135255015+chtrlir@users.noreply.github.com>
2025-07-02 12:11:27 +0530

    route changes

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import '../App.css'
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className='header-container'>
-      <div className='header-label'>TheStageRoom</div>
+      <div className='header-label'> <nav>
+        <Link to="/" end className="nav-link">TheStageRoom</Link>
+      </nav>
+             </div>
       <div className="header-right">
+        <nav>
         <ul>
-            <li>Home</li>
-             <li>About</li>
-              <li>Log in</li>
+              <Link to="/" end className="nav-link">
+              <li>
+                Home
+              </li>
+          
+        </Link>
+        <Link to="/about" end className="nav-link">
+                <li>About</li>
+          
+        </Link>
+        <Link to="/login" end className="nav-link">
+               <li>Log in</li>
+          
+        </Link>
+           
+           
+           
         </ul>
+        </nav>
       </div>
     </div>
   )
 }
 
 export default Header
-    
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Hello from login</h1>
+    <div className='login'>
+    
+      <div className="login-wrapper">
+      
+          <h1>Login</h1>
+        <div className="input-wrapper">
+          <label htmlFor="">Username &nbsp;</label>
+          <input type="text" />
+        </div>
+         <div className="passwprd-wrapper">
+          <label htmlFor="">Password &nbsp;</label>
+          <input type="password" />
+        </div>
+        <button>Submit</button>
+      </div>
+     
     </div>
   )
 }
